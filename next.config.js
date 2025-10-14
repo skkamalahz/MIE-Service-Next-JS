3881c545aa30554c0f53c2310aa64a03a33f6b11
SK Kamal AHZ Web Dev <kamal@ukcoursehunter.com>
2025-10-14 13:29:20 +0600

    netlify update

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'standalone',
+  images: {
+    unoptimized: true,
+  },
+  reactStrictMode: true,
+}
+module.exports = nextConfig
